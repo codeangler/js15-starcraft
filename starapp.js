ac056096ad5330f17105ed63a79b12ee1a18fd99
Casey Burnett <burnett.casey@gmail.com>
2016-06-03 14:41:02 -0600

    access to .json

--- starapp.js
+++ starapp.js
@@ -13,3 +13,11 @@ function starCtrl($http) {
 		console.log(starCtrl.starData.data)
 	})
 }
+// =================== ^^ Access JSON   ^^  ============
+
+// pagination http://jasonwatmore.com/post/2016/01/31/AngularJS-Pagination-Example-with-Logic-like-Google.aspx
+// pagination http://www.michaelbromley.co.uk/blog/108/paginate-almost-anything-in-angularjs
+
+// angular.module('myStarApp')
+// 	.controller('StarPagination', pagiCtrl)
+// 	StarPagination.$inject = ['']
