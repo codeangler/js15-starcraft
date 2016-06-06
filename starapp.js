bdaf4c85244691a46eb2f1e27171be54b53c98e8
Casey Burnett <burnett.casey@gmail.com>
2016-06-06 11:44:57 -0600

    add bootstrap.css

--- starapp.js
+++ starapp.js
@@ -3,21 +3,36 @@ angular.module('myStarApp')
 angular.module('myStarApp')
 	.controller('StarController',['$http', starCtrl])
 
-function starCtrl($http) {
-	console.log("starCtrl");
-	var starCtrl = this;
-	$http.get('starCraftData.json').success(function(res){
-		starCtrl.starData = res;
-		// console.log(res.cols);
-		// console.log(res.data);
-		console.log(starCtrl.starData.data)
-	})
-}
+		function starCtrl($http) {
+			console.log("starCtrl");
+			var starCtrl = this;
+			$http.get('starCraftData.json').success(function(res){
+				starCtrl.starData = res;
+				// console.log(res.cols);
+				// console.log(res.data);
+				console.log(starCtrl.starData.data)
+			})
+	}
 // =================== ^^ Access JSON   ^^  ============
 
 // pagination http://jasonwatmore.com/post/2016/01/31/AngularJS-Pagination-Example-with-Logic-like-Google.aspx
 // pagination http://www.michaelbromley.co.uk/blog/108/paginate-almost-anything-in-angularjs
 
-// angular.module('myStarApp')
-// 	.controller('StarPagination', pagiCtrl)
-// 	StarPagination.$inject = ['']
+angular.module('myStarApp')
+.controller('PaginationDemoCtrl', function ($scope, $log) {
+  $scope.totalItems = 90;
+  $scope.currentPage = 4;
+
+  $scope.setPage = function (pageNo) {
+    $scope.currentPage = pageNo;
+  };
+
+  $scope.pageChanged = function() {
+    $log.log('Page changed to: ' + $scope.currentPage);
+  };
+
+  $scope.maxSize = 5;
+  $scope.bigTotalItems = 175;
+  $scope.bigCurrentPage = 1;
+});
+
