624c30c871831b8f60a684184402e8b482c88792
Casey Burnett <burnett.casey@gmail.com>
2016-06-06 17:38:27 -0600

    Callback for inner function of controller to manage data access

--- app.paginationCtrl.js
+++ app.paginationCtrl.js
@@ -3,20 +3,20 @@ angular.module('myStarApp')
 	paginationDemoCtrl.$inject = ['StarFactory'];
 		function paginationDemoCtrl(StarFactory){
 			var pagiCtrl = this;
-			// pagiCtrl.viewby = 10;
-			// pagiCtrl.currentPage = 4;
-			// pagiCtrl.totalItems = star.starData.length;
-			// pagiCtrl.itemsPerPage = pagiCtrl.viewby;
-			// pagiCtrl.maxSize = 5; //Number of pager buttons to show
-		 //    pagiCtrl.setPage = function (pageNo) {
-			//       pagiCtrl.currentPage = pageNo;
-		 //  		}
+			pagiCtrl.viewby = 10;
+			pagiCtrl.currentPage = 4;
+			pagiCtrl.itemsPerPage = pagiCtrl.viewby;
+			pagiCtrl.maxSize = 5; //Number of pager buttons to show
+			pagiCtrl.setPage = function (pageNo) {
+			       pagiCtrl.currentPage = pageNo;
+			}
 
 		  	pagiCtrl.accessData = StarFactory.then(function(resp){
 				pagiCtrl.starData = resp.data;
 				console.log(pagiCtrl.starData);
 				console.log(pagiCtrl.starData.cols);
 				console.log("from pagiCtrl", pagiCtrl.starData.data.length);
+				pagiCtrl.totalItems = pagiCtrl.starData.data.length;
 			})
-			
+
 	  	}
