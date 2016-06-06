27a9f44061b6922473ee60f0ffe3f357a41c815b
Casey Burnett <burnett.casey@gmail.com>
2016-06-06 17:03:07 -0600

    inject 'StarFactory'

--- app.paginationCtrl.js
+++ app.paginationCtrl.js
@@ -1,14 +1,21 @@
 angular.module('myStarApp')
 	.controller('PaginationDemoCtrl', paginationDemoCtrl) 
+	paginationDemoCtrl.$inject = ['StarFactory']
+		function paginationDemoCtrl($log, StarFactory){
+			var pagiCtrl = this;
+			$scope.viewby = 10;
+			$scope.totalItems = star.starData.length;
+			$scope.currentPage = 4;
+			$scope.itemsPerPage = $scope.viewby;
+			$scope.maxSize = 5; //Number of pager buttons to show
+		    $scope.setPage = function (pageNo) {
+			      $scope.currentPage = pageNo;
+		  		}
+		  	starCtrl.accessData = StarFactory.then(function(resp){
+				starCtrl.starData = resp.data;
+				console.log(starCtrl.starData);
+				console.log(starCtrl.starData.cols);
+				console.log(starCtrl.starData.data.length);
 
-		function paginationDemoCtrl($scope, $log){
-		$scope.totalItems = 90;
-		$scope.viewby = 10;
-		$scope.totalItems = star.starData.length;
-		$scope.currentPage = 4;
-		$scope.itemsPerPage = $scope.viewby;
-		$scope.maxSize = 5; //Number of pager buttons to show
-	    $scope.setPage = function (pageNo) {
-		      $scope.currentPage = pageNo;
-	  		}
+			})
 	  	}
